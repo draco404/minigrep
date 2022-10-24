3997b64df7c96d4f1759b3e4343add6f88199c04
Alejandro Vieyra <alejandrovieyra7@gmail.com>
2022-10-24 17:43:28 -0500

    Add copy to clipboard flag

--- src/args.rs
+++ src/args.rs
@@ -4,19 +4,26 @@ pub struct Config {
     pub query: String,
     pub file_path: String,
     pub ignore_case: bool,
+    pub copy_to_clipboard: bool,
 }
 
 impl Config {
     pub fn new() -> Result<Config, &'static str> {
         let matches = command!()
             .version("1.0")
-            .author("Alejandro V. <alejandrovieyraa7@gmail.com>") // requires `cargo` feature
+            .author("Alejandro V. <alejandrovieyraa7@gmail.com>")
             .arg(arg!([query] "Word or regex to find").required(true))
             .arg(arg!([file_path] "File path or folder path").required(false))
             .arg(
                 arg!([ignore_sensitive] "Ignore case sensitive")
                     .required(false)
                     .short('i')
+                    .action(ArgAction::SetTrue)
+            )
+            .arg(
+                arg!([copy_to_clipboard] "Copy to clipboard")
+                    .required(false)
+                    .short('c')
                     .action(ArgAction::SetTrue)
             )
             .get_matches();
@@ -31,6 +38,7 @@ impl Config {
         }.to_string();
 
         let ignore_case = matches.get_flag("ignore_sensitive");
-        Ok(Config { query, file_path, ignore_case })
+        let copy_to_clipboard = matches.get_flag("copy_to_clipboard");
+        Ok(Config { query, file_path, ignore_case, copy_to_clipboard })
     } 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use std::error::Error;
+use std::{error::Error, process};
 use std::fs;
 use colored::Colorize;
-pub mod args;
+use arboard::Clipboard;
+
 mod search;
+pub mod args;
 use search::{match_regex, search};
 use args::*;
 
@@ -10,12 +12,19 @@ use args::*;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
     let results = search(&config.query, &contents, config.ignore_case);
-
-    for line in results {
+    for line in &results {
         let (start, end, text) = match_regex(line, &config.query);
         let mut line = line.to_string();
         line.replace_range(start..end, &text.red().to_string());
         println!("{}", line);
+    }
+    if config.copy_to_clipboard {
+        let text = results.join("\n");
+        let mut clipboard = Clipboard::new().unwrap_or_else(|err| {
+            eprintln!("{}", err);
+            process::exit(1);
+        });
+        clipboard.set_text(text).unwrap_or_else(|err| eprintln!("{}", err));
     }
     Ok(())
 }
